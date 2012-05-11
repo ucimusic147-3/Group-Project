3a77e1315f4ca03d5cdd4f5cf474ce7314182d8e
ucimusic147-3 <SIAVASHM@UCI.EDU>
2012-05-11 13:03:21 -0400

    Committing initial Xcode project.

---
+++ Group 3 Project/Group 3 ProjectTests/Group_3_ProjectTests.h
@@ -0,0 +1,13 @@
+//
+//  Group_3_ProjectTests.h
+//  Group 3 ProjectTests
+//
+//  Created by Kojiro Umezaki on 5/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Group_3_ProjectTests : SenTestCase
+
+@end
---
+++ Group 3 Project/Group 3 ProjectTests/Group_3_ProjectTests.m
@@ -0,0 +1,32 @@
+//
+//  Group_3_ProjectTests.m
+//  Group 3 ProjectTests
+//
+//  Created by Kojiro Umezaki on 5/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Group_3_ProjectTests.h"
+
+@implementation Group_3_ProjectTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Group 3 ProjectTests");
+}
+
+@end
