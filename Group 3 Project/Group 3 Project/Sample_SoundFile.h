ccaf99171e344a8d6ce3aaaacb4a113e908a7b54
siavashm <siavashm@dhcp209b-181.arts.uci.edu>
2012-06-13 14:12:50 -0700

    Sample_SoundFile

---
+++ Group 3 Project/Group 3 Project/Sample_SoundFile.h
@@ -0,0 +1,31 @@
+//
+//  Sample_SoundFile.h
+//  Music147_2012
+//
+//  Created by Kojiro Umezaki on 5/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <AudioToolbox/AudioToolbox.h>
+
+#import "Sample.h"
+
+/* the maximum number of samples we can read at one time */
+#define kMaxIOBufferSamples	1024
+
+@interface Sample_SoundFile : Sample {
+    
+    /* a kind of system pointer to the audio file */
+    AudioFileID		fileID;
+    
+    /* the buffer which will contain data read from the audio file */
+    SInt16			fileBuffer[kMaxIOBufferSamples];
+    
+    /* the index where the next read in the file will happen  */
+    Float64			filePos;
+}
+
+/* calling this will read the next buffer of samples */
+-(void)fillSampleBuffer:(Float64*)buffer:(UInt32)num_buf_samples;
+
+@end
