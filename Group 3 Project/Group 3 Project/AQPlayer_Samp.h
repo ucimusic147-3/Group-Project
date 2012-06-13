8d12807a6d3c1d80ae9be3c78f52dd3b35113336
siavashm <siavashm@dhcp209b-181.arts.uci.edu>
2012-06-13 14:15:49 -0700

    AQPlayer_Samp

---
+++ Group 3 Project/Group 3 Project/AQPlayer_Samp.h
@@ -0,0 +1,20 @@
+//
+//  AQPlayer_Samp.h
+//  Music147_2012
+//
+//  Created by Kojiro Umezaki on 5/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AQPlayer.h"
+#import "Sample.h"
+
+@interface AQPlayer_Samp : AQPlayer {
+    Sample* sample;
+}
+
+-(void)fillAudioBuffer:(Float64*)buffer:(UInt32)num_samples;
+
+-(void)resetStartPos;
+
+@end
